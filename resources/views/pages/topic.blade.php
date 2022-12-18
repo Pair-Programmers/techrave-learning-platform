e446241e3ad361ea288252ca9da6bcef267b7ffd
Hamza Saqib <mianhamza7262@gmail.com>
2022-12-18 18:04:27 +0500

    topic page added

--- resources/views/pages/topic.blade.php
+++ resources/views/pages/topic.blade.php
@@ -7,107 +7,103 @@
 @endsection
 
 @section('page-content')
-<div class="pcoded-content">
-    <div class="pcoded-inner-content">
-        <div class="main-body">
-            <div class="page-wrapper">
+    <div class="pcoded-content">
+        <div class="pcoded-inner-content">
+            <div class="main-body">
+                <div class="page-wrapper">
 
-                <div class="page-body">
-                    <div class="row">
-                        <!-- task, page, download counter  start -->
-                        <div class="col-xl-3 col-md-6">
-                            <div class="card bg-c-yellow update-card">
-                                <div class="card-block">
-                                    <div class="row align-items-end">
-                                        <div class="col-8">
-                                            <h4 class="text-white">$30200</h4>
-                                            <h6 class="text-white m-b-0">All Earnings</h6>
-                                        </div>
-                                        <div class="col-4 text-right">
-                                            <canvas id="update-chart-1" height="50"></canvas>
+                    <!-- Page-body start -->
+                    <div class="page-body">
+                        <!-- Row start -->
+                        <div class="row">
+                            <!-- Multiple Open Accordion start -->
+                            <div class="col-lg-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="card-header-text">All Topics</h5>
+                                    </div>
+                                    <div class="card-block accordion-block">
+                                        <div id="accordion" role="tablist" aria-multiselectable="true">
+                                            <div class="accordion-panel">
+                                                <div class="accordion-heading" role="tab" id="headingOne">
+                                                    <div class="card-title accordion-title row">
+                                                        <a class="accordion-msg col-sm-10" data-toggle="collapse"
+                                                            data-parent="#accordion" href="#collapseOne"
+                                                            aria-expanded="true" aria-controls="collapseOne">
+                                                            Heading 1
+                                                        </a>
+                                                        <div class="com-sm-2" style="border-top: solid; display:flex">
+                                                            <h2>asd</h2>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel"
+                                                    aria-labelledby="headingOne">
+                                                    <div class="accordion-content accordion-desc">
+                                                        <div class="col-sm-12">
+                                                            <textarea rows="5" cols="5" class="form-control" placeholder="write from here.."></textarea>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="accordion-panel">
+                                                <div class="accordion-heading" role="tab" id="headingTwo">
+                                                    <h3 class="card-title accordion-title">
+                                                        <a class="accordion-msg" data-toggle="collapse"
+                                                            data-parent="#accordion" href="#collapseTwo"
+                                                            aria-expanded="false" aria-controls="collapseTwo">
+                                                            Heading 2
+                                                        </a>
+                                                    </h3>
+                                                </div>
+                                                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel"
+                                                    aria-labelledby="headingTwo">
+                                                    <div class="accordion-content accordion-desc">
+                                                        <div class="col-sm-12">
+                                                            <textarea rows="5" cols="5" class="form-control" placeholder="write from here.."></textarea>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="accordion-panel">
+                                                <div class=" accordion-heading" role="tab" id="headingThree">
+                                                    <h3 class="card-title accordion-title">
+                                                        <a class="accordion-msg" data-toggle="collapse"
+                                                            data-parent="#accordion" href="#collapseThree"
+                                                            aria-expanded="false" aria-controls="collapseThree">
+                                                            Heading 3
+                                                        </a>
+                                                    </h3>
+                                                </div>
+                                                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel"
+                                                    aria-labelledby="headingThree">
+                                                    <div class="accordion-content accordion-desc">
+                                                        <div class="col-sm-12">
+                                                            <textarea rows="5" cols="5" class="form-control" placeholder="write from here.."></textarea>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-footer">
-                                    <p class="text-white m-b-0"><i
-                                            class="feather icon-clock text-white f-14 m-r-10"></i>update
-                                        : 2:15 am</p>
-                                </div>
                             </div>
+                            <!-- Multiple Open Accordion ends -->
                         </div>
-                        <div class="col-xl-3 col-md-6">
-                            <div class="card bg-c-green update-card">
-                                <div class="card-block">
-                                    <div class="row align-items-end">
-                                        <div class="col-8">
-                                            <h4 class="text-white">290+</h4>
-                                            <h6 class="text-white m-b-0">Page Views</h6>
-                                        </div>
-                                        <div class="col-4 text-right">
-                                            <canvas id="update-chart-2" height="50"></canvas>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card-footer">
-                                    <p class="text-white m-b-0"><i
-                                            class="feather icon-clock text-white f-14 m-r-10"></i>update
-                                        : 2:15 am</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-md-6">
-                            <div class="card bg-c-pink update-card">
-                                <div class="card-block">
-                                    <div class="row align-items-end">
-                                        <div class="col-8">
-                                            <h4 class="text-white">145</h4>
-                                            <h6 class="text-white m-b-0">Task Completed</h6>
-                                        </div>
-                                        <div class="col-4 text-right">
-                                            <canvas id="update-chart-3" height="50"></canvas>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card-footer">
-                                    <p class="text-white m-b-0"><i
-                                            class="feather icon-clock text-white f-14 m-r-10"></i>update
-                                        : 2:15 am</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-md-6">
-                            <div class="card bg-c-lite-green update-card">
-                                <div class="card-block">
-                                    <div class="row align-items-end">
-                                        <div class="col-8">
-                                            <h4 class="text-white">500</h4>
-                                            <h6 class="text-white m-b-0">Downloads</h6>
-                                        </div>
-                                        <div class="col-4 text-right">
-                                            <canvas id="update-chart-4" height="50"></canvas>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card-footer">
-                                    <p class="text-white m-b-0"><i
-                                            class="feather icon-clock text-white f-14 m-r-10"></i>update
-                                        : 2:15 am</p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- task, page, download counter  end -->
-
+                        <!-- Row end -->
                     </div>
+                    <!-- Page-body end -->
                 </div>
-            </div>
 
-            <div id="styleSelector">
+                <div id="styleSelector">
 
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('custom-script')
+    <!-- Accordion js -->
+    <script type="text/javascript" src="{{ asset('assets') }}/pages/accordion/accordion.js"></script>
 @endsection
